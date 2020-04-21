608bc89f35618b37b0e74dc8061ffaaab7c9ad06
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-04-21 12:55:48 -0700

    added axiosWithAuth component, and the PrivateRoute comonent

--- friends/src/components/MainApp.js
+++ friends/src/components/MainApp.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import PrivateRoute from "../protected/ProtectedRoute";
+import MainForm from "./MainForm";
 
 const MainApp = () => {
+  const dispatch = useDispatch();
+  const reducer = useSelector((state) => ({
+    ...state,
+  }));
+
+  const { data } = reducer.formReducer;
+  //   console.log(data);
   return (
-    <div>
+    <div className="MainForm">
       <h1>MainApp</h1>
+      <MainForm />
     </div>
   );
 };
--- friends/src/components/MainForm.js
+++ friends/src/components/MainForm.js
@@ -1,10 +1,50 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { Formik, Form, Field } from "formik";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const MainForm = () => {
+  const history = useHistory();
   return (
     <div>
-      <h3>MainForm</h3>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values, { resetForm }) => {
+          axiosWithAuth()
+            .post("/api/login", values)
+            .then((res) => {
+              console.log("response here ", res);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+          resetForm();
+        }}
+      >
+        {() => (
+          <Form>
+            <label htmlFor="username">
+              <Field
+                type="text"
+                name="username"
+                id="username"
+                placeholder="username"
+              />
+            </label>
+            <label htmlFor="password">
+              <Field
+                type="password"
+                name="password"
+                id="password"
+                placeholder="password"
+              />
+            </label>
+            <div className="btn">
+              <button type="submit">Submit</button>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
